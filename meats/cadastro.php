1c6f23a70f251fb1480079c95e4f6ad76894757f
josesedrez <30626951+josesedrez@users.noreply.github.com>
2018-06-20 21:08:39 -0300

    Add files via upload

--- meats/cadastro.php
+++ meats/cadastro.php
@@ -8,8 +8,6 @@ require_once('inc/cabecalho.php');
 	<input type="email" name="email" id="email"><br><br>
 	<label class="text-center mb-1">Senha</label><br>
 	<input type="password" name="senha" id="senha"><br><br>
-	<label class="text-center mb-1">Confirme a senha</label><br>
-	<input type="password" name="confirma_senha" id="confirma_senha"><br><br>
 	<button type="submit" class="btn-success">Cadastrar-se</button><br><br>
 	<a href="index.php">Voltar</a>
 </form>
--- meats/inc/conexao.php
+++ meats/inc/conexao.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$base_path = 'C:/Bitnami/wappstack-7.1.15-0/apache2/htdocs/meats/';
 $base_url='http://localhost/meats/';
 $conexao=pg_connect('host=localhost port=5432 dbname=meatsProject user=postgres password=senha5');
 if(!$conexao){
